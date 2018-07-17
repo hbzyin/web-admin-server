8d8d40bab839ce121937ea552c7ce248db0ac3f7
张印 <zhangyin08@meituan.com>
2018-07-17 13:55:59 +0800

    新增:更新gitignore

--- db/mysql.js
+++ db/mysql.js
@@ -1,6 +1,6 @@
 var mysql= require('mysql');
 var connection = mysql.createConnection({
-  host: 'xx.xx.xx.xx',
+  host: '47.104.134.40',
   user: 'root',
   password: '123456',
   database: 'web_admin'
